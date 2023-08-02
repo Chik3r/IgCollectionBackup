cf9a4249321853d4bfaf727c61921b1a45aa1fce
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-08-02 00:29:38 -0400

    Download files

--- IgCollectionBackup/Program.cs
+++ IgCollectionBackup/Program.cs
@@ -5,6 +5,8 @@ using InstagramApi.Data;
 using Spectre.Console;
 
 class Program {
+    private static HttpClient Client { get; } = new();
+    
     public static async Task Main() {
         Instagram ig = new();
         
@@ -25,24 +27,44 @@ class Program {
             AnsiConsole.WriteLine($"Downloading collection: {collection.CollectionName}");
             Progress progress = AnsiConsole.Progress();
 
-            await progress.StartAsync(async ctx => {
-                ProgressTask task = ctx.AddTask("Download", maxValue: collection.CollectionMediaCount);
-
-                ItemsResponse<MediaWrapper>? collectionMedia = await ig.GetCollectionMedia(collection);
-                foreach (MediaWrapper mediaWrapper in collectionMedia.Items) {
-                    Media media = mediaWrapper.Media;
-                    string fileName = media.ImageVersions.Candidates[0].Url;
-                    task.Increment(1);
-                }
-
-                while ((collectionMedia = await ig.GetCollectionMediaNextPage(collection, collectionMedia)) != null) {
-                    foreach (MediaWrapper mediaWrapper in collectionMedia.Items) {
-                        Media media = mediaWrapper.Media;
-                        string fileName = media.ImageVersions.Candidates[0].Url;
-                        task.Increment(1);
-                    }
-                }
-            });
+            await progress.StartAsync(async ctx => await DownloadCollection(collection, ig, ctx));
         }
+    }
+
+    private static async Task DownloadCollection(Collection collection, Instagram ig, ProgressContext ctx) {
+        ProgressTask task = ctx.AddTask("Download", maxValue: collection.CollectionMediaCount);
+
+        ItemsResponse<MediaWrapper>? collectionMedia = await ig.GetCollectionMedia(collection);
+        foreach (MediaWrapper mediaWrapper in collectionMedia.Items) {
+            Media media = mediaWrapper.Media;
+            await DownloadFile(Client, media);
+            
+            task.Increment(1);
+        }
+
+        while ((collectionMedia = await ig.GetCollectionMediaNextPage(collection, collectionMedia)) != null) {
+            foreach (MediaWrapper mediaWrapper in collectionMedia.Items) {
+                Media media = mediaWrapper.Media;
+                await DownloadFile(Client, media);
+                
+                task.Increment(1);
+            }
+        }
+    }
+
+    private static async Task DownloadFile(HttpClient client, Media media) {
+        string webFilename = media.ImageVersions.Candidates[0].Url;
+        byte[] byteArray = await client.GetByteArrayAsync(webFilename);
+
+        Uri uri = new(webFilename);
+        string extension = Path.GetExtension(uri.LocalPath);
+        
+        DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(media.TakenAt);
+        string timeText = time.ToString("yyyyMMddTHHmm");
+        
+        string fileName = $"{media.User.Username} - {timeText}{extension}";
+
+        Image image = Image.Load(byteArray);
+        await image.SaveAsync(fileName);
     }
 }
