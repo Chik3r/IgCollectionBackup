22fe9f9e1be5d6b5f2efd99a177df8006e0a0697
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-08-01 23:21:37 -0400

    testing ig thing

--- IgCollectionBackup/Program.cs
+++ IgCollectionBackup/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using InstagramApi;
+using InstagramApi.Data;
+using Spectre.Console;
+
 Console.WriteLine("Hello, World!");
+
+Instagram ig = new();
+
+ItemsResponse<Collection> tmp = await ig.GetCollections();
+AnsiConsole.WriteLine(tmp.ToString());
+
+foreach (Collection collection in tmp.Items) {
+    ItemsResponse<MediaWrapper> tmp2 = await ig.GetCollectionMedia(collection);
+    Console.WriteLine(tmp2);
+
+    ItemsResponse<MediaWrapper>? nextPage;
+    while ((nextPage = await ig.GetCollectionMediaNextPage(collection, tmp2)) != null) {
+        Console.WriteLine(nextPage);
+    }
+}
