3a92f6518ea0e9a8cf943a511b77e47ad9add36a
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-08-01 23:21:16 -0400

    Get collection next pages

--- InstagramApi/Instagram.cs
+++ InstagramApi/Instagram.cs
@@ -23,15 +23,60 @@ public class Instagram {
     public async Task<ItemsResponse<Collection>> GetCollections() {
         HttpRequestMessage request = new(HttpMethod.Get,
             "https://www.instagram.com/api/v1/collections/list/?collection_types=%5B%22ALL_MEDIA_AUTO_COLLECTION%22%2C%22MEDIA%22%2C%22AUDIO_AUTO_COLLECTION%22%5D&include_public_only=0&max_id=");
-        request.Headers.Add("Cookie", Cookie);
-        request.Headers.Add("x-asbd-id", AsbdId);
-        request.Headers.Add("x-csrftoken", CrsfToken);
-        request.Headers.Add("x-ig-app-id", IgAppId);
-        request.Headers.Add("x-ig-www-claim", IgWwwClaim);
+        AddRequiredHeaders(ref request);
         
         HttpResponseMessage response = await Client.SendAsync(request);
         string content = await response.Content.ReadAsStringAsync();
         
         return JsonSerializer.Deserialize<ItemsResponse<Collection>>(content)!;
+    }
+
+    public Task<ItemsResponse<MediaWrapper>> GetCollectionMedia(Collection collection) =>
+        GetCollectionMedia(collection.CollectionId);
+
+    public async Task<ItemsResponse<MediaWrapper>> GetCollectionMedia(string collectionId) {
+        string uri;
+        if (collectionId == "ALL_MEDIA_AUTO_COLLECTION")
+            uri = "https://www.instagram.com/api/v1/feed/saved/posts/";
+        else
+            uri = $"https://www.instagram.com/api/v1/feed/collection/{collectionId}/posts/";
+        
+        HttpRequestMessage request = new(HttpMethod.Get, uri);
+        AddRequiredHeaders(ref request);
+        
+        HttpResponseMessage response = await Client.SendAsync(request);
+        string content = await response.Content.ReadAsStringAsync();
+        
+        return JsonSerializer.Deserialize<ItemsResponse<MediaWrapper>>(content)!;
+    }
+    
+    public Task<ItemsResponse<MediaWrapper>?> GetCollectionMediaNextPage(Collection collection, ItemsResponse<MediaWrapper> previousPage) =>
+        GetCollectionMediaNextPage(collection.CollectionId, previousPage);
+
+    public async Task<ItemsResponse<MediaWrapper>?> GetCollectionMediaNextPage(string collectionId, ItemsResponse<MediaWrapper> previousPage) {
+        if (!previousPage.MoreAvailable) return null;
+
+        string uri;
+        if (collectionId == "ALL_MEDIA_AUTO_COLLECTION")
+            uri = "https://www.instagram.com/api/v1/feed/saved/posts/";
+        else
+            uri = $"https://www.instagram.com/api/v1/feed/collection/{collectionId}/posts/?max_id=";
+        uri += $"?max_id={previousPage.NextMaxId}";
+        
+        HttpRequestMessage request = new(HttpMethod.Get, uri);
+        AddRequiredHeaders(ref request);
+        
+        HttpResponseMessage response = await Client.SendAsync(request);
+        string content = await response.Content.ReadAsStringAsync();
+        
+        return JsonSerializer.Deserialize<ItemsResponse<MediaWrapper>>(content)!;
+    }
+
+    private void AddRequiredHeaders(ref HttpRequestMessage request) {
+        request.Headers.Add("Cookie", Cookie);
+        request.Headers.Add("x-asbd-id", AsbdId);
+        request.Headers.Add("x-csrftoken", CrsfToken);
+        request.Headers.Add("x-ig-app-id", IgAppId);
+        request.Headers.Add("x-ig-www-claim", IgWwwClaim);
     }
 }
