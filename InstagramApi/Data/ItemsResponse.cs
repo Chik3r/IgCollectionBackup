100de4f1a0c83cb9ec7b98cc41334c69e933c7df
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-08-01 23:21:25 -0400

    Add nextmaxid

--- InstagramApi/Data/ItemsResponse.cs
+++ InstagramApi/Data/ItemsResponse.cs
@@ -4,7 +4,8 @@ namespace InstagramApi.Data;
 
 public record ItemsResponse<T>(
     [property: JsonPropertyName("items")] IReadOnlyList<T> Items,
-    [property: JsonPropertyName("num_results") ]int? NumResults,
+    [property: JsonPropertyName("num_results")] int? NumResults,
+    [property: JsonPropertyName("next_max_id")] string? NextMaxId,
     [property: JsonPropertyName("more_available")] bool MoreAvailable,
     [property: JsonPropertyName("auto_load_more_enabled")] bool AutoLoadMoreEnabled,
     [property: JsonPropertyName("status")] string Status
